6ce8f75e25312a55c71548d836d56746e6c694cc
fraxi0n <f.brias@laposte.net>
2023-12-20 12:14:08 +0100

    footer align

--- client/src/component/Footer.tsx
+++ client/src/component/Footer.tsx
@@ -10,19 +10,19 @@ const Footer = () => {
             <footer>
                 <div className='footer-content'>
                     <div className='footer-container' >
-                        <div>
+                        <div className='footer-container-2'>
                             Créateur : <h2> françois BRIAS  </h2>
                         </div>
-                        <div>
+                        <div className='footer-container-2'>
                             site internet : <a href="https://francois-brias.xyz"> francois-brias.xyz </a> <br></br>
                             prototype : <a href="https://half-squid.itch.io/larcin-lazer-challenges"> larcin-lazer-challenges </a>
                         </div>
                     </div>
                     <div className='footer-container' >
-                        <div>
+                        <div className='footer-container-2'>
                             concept original: <h2> Géraud Zuchini (@DrGeraud) </h2>
                         </div>
-                        <div>
+                        <div className='footer-container-2'>
                             studio : <a href="https://tambouillestudio.com/"> tambouille studio </a>
                         </div>
                     </div>
